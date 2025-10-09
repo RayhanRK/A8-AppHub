3fa392b2f52a9d6134b7b4489347a5489a2181fb
RayhanRK <m.rayhankhan01@gmail.com>
2025-10-09 23:05:18 +0600

    Some minor bug fixed

--- src/RootLayout/RootLayout.jsx
+++ src/RootLayout/RootLayout.jsx
@@ -7,21 +7,6 @@ import Spinner from "./Spinner";
 
 
 const RootLayout = () => {
-//   return (
-//     <div className="flex flex-col min-h-screen">
-//       <header className="lg:px-8 py-2 md:px-4 px-2">
-//         <Navbar />
-//       </header>
-//               {/* bg-[#E5E7EB] */}
-//       <div className="bg-gray-100 flex-1">
-//           <Outlet />
-//       </div>
-
-//       <Footer />
-//       <ToastContainer />
-//     </div>
-//   );
-// };
 
 const location = useLocation();
     const [loading, setLoading] = useState(true);
